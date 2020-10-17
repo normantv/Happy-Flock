5f966b468ce29ce2b7c7faf1ed7225b51eb8bd31
Norman A. Toro Vega <normant@bu.edu>
2020-10-17 14:55:16 -0400

    Initial Commit

---
+++ Happy Flock/Happy_FlockApp.swift
@@ -0,0 +1,17 @@
+//
+//  Happy_FlockApp.swift
+//  Happy Flock
+//
+//  Created by Norman A. Toro Vega on 10/17/20.
+//
+
+import SwiftUI
+
+@main
+struct Happy_FlockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
